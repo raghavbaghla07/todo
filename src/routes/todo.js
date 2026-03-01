da2538bf1f812b5a6611f999778b7dc568677efc
Raghav Baghla <raghavbaghla.cse@gmail.com>
2026-03-01 12:44:01 +0530

    find todo by id, delete todo, update todo

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -3,6 +3,7 @@ const todoRouter = express.Router();
 const { userAuth } = require("../middlewares/auth")
 const Todo = require("../models/todo")
 
+//crate a todo
 todoRouter.post("/todo", userAuth, async (req, res) => {
     try {
         const { title, description } = req.body;
@@ -18,7 +19,7 @@ todoRouter.post("/todo", userAuth, async (req, res) => {
     }
 })
 
-
+//get all todo
 todoRouter.get("/todos", userAuth, async (req, res) => {
     try {
         const todos = await Todo.find({
@@ -30,6 +31,51 @@ todoRouter.get("/todos", userAuth, async (req, res) => {
     }
 })
 
+// get todo by id
+todoRouter.get("/todo/:id", userAuth, async (req, res) => {
+    try {
+        const todo = await Todo.findOne({
+            userId: req.user._id,
+            _id: req.params.id
+        })
+        if (!todo)
+            res.status(404).send("todo not found");
+        res.send(todo);
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message)
+    }
+})
+
+//update todo
+todoRouter.patch("/todo/:id", userAuth, async (req, res) => {
+    try {
+        const updatedTodo = await Todo.findOneAndUpdate({
+            userId: req.user._id,
+            _id: req.params.id
+        },
+            req.body,
+            { new: true }
+        );
+        res.send(updatedTodo);
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message)
+    }
+})
+
+// delete todo
+todoRouter.delete("/todo/:id", userAuth, async (req, res) => {
+    try {
+        const deleteTodo = await Todo.findOneAndDelete({
+            userId: req.user._id,
+            _id: req.params.id
+        });
+        if (!deleteTodo)
+            return res.status(404).send("Todo does not exist")
+        res.send("todo deleted successfully")
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message)
+    }
+})
 module.exports = todoRouter;
 
 
