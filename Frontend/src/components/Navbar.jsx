f3e31442910dd8035602481dbaf5fc11bd5ad556
Raghav Baghla <raghavbaghla.cse@gmail.com>
2026-03-02 13:49:59 +0530

    navbar bug resolve

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -15,9 +15,7 @@ const Navbar = () => {
 
   return (
     <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <button className="btn btn-ghost font-bold text-sm ml-20"></button>
-      </div>
+      <div className="navbar-start"></div>
       <div className="navbar-center">
         <a className="btn btn-ghost text-xl">Your task manager</a>
       </div>
